b29fd41b898718342c942d8f9f5719f39123d78e
IvanBrasilico <brasilico.ivan@gmail.com>
2020-10-07 19:17:25 -0300

    Criar classe MySQL para GMCI e código inicial para salvar dia de ontem

--- virasana/integracao/jupiapi.py
+++ virasana/integracao/jupiapi.py
@@ -1,11 +1,15 @@
 """Funções para leitura e tratamento dos dados de pesagem e gate dos recintos.
 """
-import csv
 import os
 from datetime import date, datetime, timedelta
 
 import requests
+from ajna_commons.flask.conf import SQL_URI
 from ajna_commons.flask.log import logger
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from integracao.gmci_alchemy import GMCI
 
 DTE_USERNAME = os.environ.get('DTE_USERNAME')
 DTE_PASSWORD = os.environ.get('DTE_PASSWORD')
@@ -31,7 +35,7 @@ def get_token_api(username=DTE_USERNAME, password=DTE_PASSWORD):
     r = requests.post(API_TOKEN, data=data, verify=False)
     print(r.url)
     print(r.text)
-    print(data)
+    # print(data)
     print(r.status_code)
     token = r.json().get('access_token')
     return token
@@ -43,7 +47,7 @@ def get_gmci(datainicial, datafinal, token):
     headers = {'Authorization': 'Bearer ' + token}
     print(payload)
     r = requests.post(GMCI_URL, headers=headers, data=payload, verify=False)
-    logger.debug('get_pesagens_dte ' + r.url)
+    logger.debug('get_gmci ' + r.url)
     try:
         lista_pesagens = r.json()
         return lista_pesagens
@@ -52,8 +56,25 @@ def get_gmci(datainicial, datafinal, token):
 
 
 if __name__ == '__main__':  # pragma: no cover
-    start = datetime.combine(date.today(), datetime.min.time())
+    engine = create_engine(SQL_URI)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    start = datetime.combine(date.today(), datetime.min.time()) - timedelta(days=1)
     end = datetime.now()
     token = get_token_api()
     print(token)
-    print(get_gmci(start, end, token))
+    gmcis_dict = get_gmci(start, end, token)
+    for gmci_dict in gmcis_dict:
+        gmci = GMCI()
+        gmci.cod_recinto = gmci_dict['cod_recinto']
+        gmci.num_gmci = gmci_dict['num_gmci']
+        gmci.num_conteiner = gmci_dict['num_conteiner']
+        datahora = gmci_dict['data_dt'] + gmci_dict['hora_dt']
+        gmci.datahora = datetime.strptime()
+        try:
+            session.add(gmci)
+            session.commit()
+        except Exception as err:
+            logger.error(err)
+            session.rollback()
+
