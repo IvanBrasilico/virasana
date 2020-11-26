5958069071f748dea557594df25740fe270c4c0d
IvanBrasilico <brasilico.ivan@gmail.com>
2020-11-26 11:25:46 -0300

    MySQL estava redundante

--- virasana/main.py
+++ virasana/main.py
@@ -21,8 +21,8 @@ log.logger.info('Servidor (re)iniciado!')
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
-    db_session.remove()
-    log.logger.info('db_session remove')
+    # db_session.remove()
+    log.logger.info('db_session remove (não ativado - ver se é necessário ativar)')
 
 
 
