cd6941ce8641dc4f473525a546afb5fea4cb62a8
IvanBrasilico <brasilico.ivan@gmail.com>
2022-10-20 16:57:35 -0300

    Nova regra vermelho

--- virasana/integracao/bagagens/regra_vermelho_portaria.py
+++ virasana/integracao/bagagens/regra_vermelho_portaria.py
@@ -12,24 +12,22 @@ então colocar em canal vermelho SE o dígito anterior for
 from datetime import date
 
 segundo_digito_valido = {
-    '1': '01',
-    '2': '23',
-    '3': '45',
-    '4': '67',
-    '5': '89',
-    '6': '01',
-    '7': '23',
-    '8': '45',
-    '9': '67',
-    '0': '89',
+    '1': '5',
+    '2': '6',
+    '3': '7',
+    '4': '8',
+    '5': '9',
+    '6': '0',
+    '7': '1',
+    '8': '2',
+    '9': '3',
+    '0': '4',
 }
 
 
 def regra_matematica_digito(digito: int, anterior: int):
-    valor = (digito % 5) * 2
-    if valor == 0:
-        valor = 10
-    return (anterior < valor) and (anterior > valor - 3)
+    valor = (digito + 4) % 10
+    return valor == anterior
 
 
 def e_canal_vermelho(num_dsi: str, data_registro: date):
@@ -51,7 +49,7 @@ if __name__ == '__main__':
 
     for digito, anteriores in segundo_digito_valido.items():
         assert regra_matematica_digito(int(digito), int(anteriores[0]))
-        assert regra_matematica_digito(int(digito), int(anteriores[1]))
+        # assert regra_matematica_digito(int(digito), int(anteriores[1]))
 
     DSIs_verdes = [
         ['2200000002', date(2022, 1, 1)],
@@ -65,8 +63,6 @@ if __name__ == '__main__':
         ['2200000074', date(2022, 1, 9)],
         ['2200000085', date(2022, 1, 10)],
         ['2200000065', date(2022, 1, 5)],
-    ]
-    DSIs_vermelhas = [
         ['2200000001', date(2022, 1, 1)],
         ['2200000001', date(2022, 1, 21)],
         ['2200000001', date(2022, 1, 31)],
@@ -78,20 +74,34 @@ if __name__ == '__main__':
         ['2200000064', date(2022, 1, 14)],
         ['2200000074', date(2022, 1, 24)],
         ['2200000085', date(2022, 1, 25)],
+    ]
+    DSIs_vermelhas = [
+        ['2200000051', date(2022, 1, 1)],
+        ['2200000051', date(2022, 1, 21)],
+        ['2200000051', date(2022, 1, 31)],
+        ['2200000051', date(2022, 1, 11)],
+        ['2200000062', date(2022, 1, 2)],
+        ['2200000062', date(2022, 1, 12)],
+        ['2200000073', date(2022, 1, 3)],
+        ['2200000073', date(2022, 1, 13)],
+        ['2200000084', date(2022, 1, 14)],
+        ['2200000084', date(2022, 1, 24)],
+        ['2200000095', date(2022, 1, 25)],
         ['2200000095', date(2022, 1, 5)],
         ['2200000006', date(2022, 1, 16)],
-        ['2200000016', date(2022, 1, 6)],
-        ['2200000027', date(2022, 1, 7)],
-        ['2200000037', date(2022, 1, 27)],
-        ['2200000048', date(2022, 1, 18)],
-        ['2200000058', date(2022, 1, 28)],
-        ['2200000069', date(2022, 1, 9)],
-        ['2200000079', date(2022, 1, 29)],
-        ['2200000080', date(2022, 1, 10)],
-        ['2200000090', date(2022, 1, 30)],
+        ['2200000006', date(2022, 1, 6)],
+        ['2200000017', date(2022, 1, 7)],
+        ['2200000017', date(2022, 1, 27)],
+        ['2200000028', date(2022, 1, 18)],
+        ['2200000028', date(2022, 1, 28)],
+        ['2200000039', date(2022, 1, 9)],
+        ['2200000039', date(2022, 1, 29)],
+        ['2200000040', date(2022, 1, 10)],
+        ['2200000040', date(2022, 1, 30)],
     ]
     for dsi in DSIs_verdes:
         assert e_canal_vermelho(dsi[0], dsi[1]) is False
 
     for dsi in DSIs_vermelhas:
+        # print(dsi)
         assert e_canal_vermelho(dsi[0], dsi[1]) is True
