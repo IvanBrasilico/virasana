a10857ab7c606d9de02864db6f0493437e372941
IvanBrasilico <brasilico.ivan@gmail.com>
2023-09-06 14:51:02 -0300

    API Recintos App - novos campos de peso na tela e na planilha

--- virasana/routes/apirecintos_app.py
+++ virasana/routes/apirecintos_app.py
@@ -3,19 +3,57 @@ from datetime import datetime
 
 import pandas as pd
 from ajna_commons.flask.log import logger
-from bhadrasana.models.apirecintos import AcessoVeiculo
+from bhadrasana.models.apirecintos import AcessoVeiculo, PesagemVeiculo
 from bhadrasana.models.apirecintos_risco import Motorista
 from bhadrasana.models.ovrmanager import get_recintos_api
 from flask import render_template, request, flash
 from flask_login import login_required, current_user
-from virasana.views import get_user_save_path
 from virasana.forms.filtros import FormFiltroAPIRecintos
 from virasana.usecases.apirecintos_manager import get_eventos, Missao
+from virasana.views import get_user_save_path
 from werkzeug.utils import redirect
 
 
 def configure(app):
     """Configura rotas para evento."""
+
+    def monta_planilha_apirecintos(lista_eventos):
+        titulos = ['recinto', 'missao',
+                   'motorista.cpf', 'motorista.nome', 'motorista.risco',
+                   'entrada.dataHoraOcorrencia',
+                   'entrada.placa', 'entrada.numeroConteiner', 'entrada.cnpjTransportador',
+                   'entrada.numeroDeclaracao', 'entrada.numeroConhecimento', 'entrada.listaNfe',
+                   'pesagem.dataHoraOcorrencia', 'pesagem.taraSemirreboque',
+                   'pesagem.pesoBrutoManifesto', 'pesagem.pesoBrutoBalanca',
+                   'saida.dataHoraOcorrencia',
+                   'saida.placa', 'saida.numeroConteiner', 'saida.cnpjTransportador',
+                   'saida.numeroDeclaracao', 'saida.numeroConhecimento', 'saida.listaNfe']
+        linhas = []
+        for evento in lista_eventos:
+            motorista: Motorista = evento['motorista']
+            entrada: AcessoVeiculo = evento['entrada']
+            pesagem: PesagemVeiculo = evento.get('pesagem', PesagemVeiculo())
+            saida: AcessoVeiculo = evento.get('saida', AcessoVeiculo())
+            if pesagem is None:
+                pesagem = PesagemVeiculo()
+            if saida is None:
+                saida = AcessoVeiculo()
+            linha = [evento['recinto'], evento['missao'],
+                     motorista.cpf, motorista.nome, motorista.get_risco(),
+                     entrada.dataHoraOcorrencia,
+                     entrada.placa, entrada.numeroConteiner, entrada.cnpjTransportador,
+                     entrada.numeroDeclaracao, entrada.numeroConhecimento, entrada.listaNfe,
+                     pesagem.dataHoraOcorrencia, pesagem.taraSemirreboque,
+                     pesagem.pesoBrutoManifesto, pesagem.pesoBrutoBalanca,
+                     saida.dataHoraOcorrencia,
+                     saida.placa, saida.numeroConteiner, saida.cnpjTransportador,
+                     saida.numeroDeclaracao, saida.numeroConhecimento, saida.listaNfe]
+            linhas.append(linha)
+        if len(linhas) > 0:
+            df = pd.DataFrame(linhas)
+            df.columns = titulos
+            return df
+        return None
 
     def lista_eventos_html(mongodb, session, form: FormFiltroAPIRecintos):
         # start = datetime.combine(form.start.data, datetime.min.time())
@@ -59,35 +97,12 @@ def configure(app):
         try:
             if request.method == 'POST':
                 lista_eventos, count_missao = lista_eventos_html(mongodb, session, oform)
-                print('************', request.values)
                 if request.values.get('planilha'):
-                    titulos = ['recinto', 'missao',
-                               'motorista.cpf', 'motorista.nome', 'motorista.risco',
-                               'entrada.placa', 'entrada.numeroConteiner', 'entrada.cnpjTransportador',
-                               'entrada.numeroDeclaracao', 'entrada.numeroConhecimento', 'entrada.listaNfe',
-                               'saida.placa', 'saida.numeroConteiner', 'saida.cnpjTransportador',
-                               'saida.numeroDeclaracao', 'saida.numeroConhecimento', 'saida.listaNfe']
-                    linhas = []
-                    for evento in lista_eventos:
-                        motorista: Motorista = evento['motorista']
-                        entrada: AcessoVeiculo = evento['entrada']
-                        saida: AcessoVeiculo = evento['saida']
-                        if saida is None:
-                            saida = AcessoVeiculo()
-                        linha = [evento['recinto'], evento['missao'],
-                                 motorista.cpf, motorista.nome, motorista.get_risco(),
-                                 entrada.placa, entrada.numeroConteiner, entrada.cnpjTransportador,
-                                 entrada.numeroDeclaracao, entrada.numeroConhecimento, entrada.listaNfe,
-                                 saida.placa, saida.numeroConteiner, saida.cnpjTransportador,
-                                 saida.numeroDeclaracao, saida.numeroConhecimento, saida.listaNfe]
-                        linhas.append(linha)
-                    if len(linhas) > 0:
-                        df = pd.DataFrame(linhas)
-                        df.columns = titulos
+                    planilha = monta_planilha_apirecintos(lista_eventos)
+                    if planilha is not None:
                         out_filename = '{}_{}.xlsx'.format('PesquisaAPIRecintos_',
-                                                          datetime.strftime(datetime.now(), '%Y-%m-%dT%H-%M-%S'))
-                        print(os.path.join(get_user_save_path(app), out_filename))
-                        df.to_excel(os.path.join(get_user_save_path(app), out_filename), index=False)
+                                                           datetime.strftime(datetime.now(), '%Y-%m-%dT%H-%M-%S'))
+                        planilha.to_excel(os.path.join(get_user_save_path(app), out_filename), index=False)
                         return redirect('static/%s/%s' % (current_user.name, out_filename))
         except Exception as err:
             flash(err)
