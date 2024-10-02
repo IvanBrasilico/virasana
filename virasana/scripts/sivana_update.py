d3d879856e283559a70014add16cc33d445f4714
IvanBrasilico <brasilico.ivan@gmail.com>
2024-10-02 17:23:53 -0300

    Exibir mensagem no log em caso de não existirem registros a transmitir

--- virasana/scripts/sivana_update.py
+++ virasana/scripts/sivana_update.py
@@ -63,17 +63,20 @@ def update(connection):
         acessos = session.query(AcessoVeiculo).filter(AcessoVeiculo.id > ultimo_id).limit(500).all()
         passagens = [acesso.to_sivana() for acesso in acessos]
         payload = {'totalLinhas': len(passagens), 'offset': '-03:00', 'passagens': passagens}
-        print(payload)
-        r = post(URL_API_SIVANA, pkcs12_filename=PKCS12_FILENAME,
-                 pkcs12_password=SENHA_PCKS_SIVANA, json=payload, verify=False)
-        if r.status_code != 200:
-            logger.error(f'ERRO {r.status_code} no Upload para Sivana: {r.text}')
-        else:
+        # print(payload)
+        # Verifica se há acessos a transmitir
+        if passagens and len(passagens) > 0:
+            r = post(URL_API_SIVANA, pkcs12_filename=PKCS12_FILENAME,
+                     pkcs12_password=SENHA_PCKS_SIVANA, json=payload, verify=False)
+            if r.status_code != 200:
+                logger.error(f'ERRO {r.status_code} no Upload para Sivana: {r.text}')
+            else:
             # Verifica se houve acessos e pega o maior ID
-            if acessos and len(acessos) > 0:
                 maior_id = max(acesso.id for acesso in acessos)
                 # Atualiza o arquivo com o maior ID encontrado
                 grava_ultimo_id_transmitido(maior_id)
+        else:
+            logger.info(f'Não há novos registros de acesso a transmitir')
     finally:
         session.close()
 
