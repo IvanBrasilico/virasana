ebca544c924a18432be5d46627f3dd8c3afb3a0b
IvanBrasilico <brasilico.ivan@gmail.com>
2025-02-12 08:52:37 -0300

    Snippet inicial para tratar endpoint da APS para Sivana, está genérico, de forma que novos endpoints só precisariam de novas classes descendentes.

--- virasana/integracao/sivana/aps_porto_de_santos.py
+++ virasana/integracao/sivana/aps_porto_de_santos.py
@@ -1,29 +1,41 @@
 import xml.etree.ElementTree as ET
+from datetime import datetime, timedelta
+
+import requests
 
 
-class APSPortodeSantos():
-
+class TratamentoLPR:
     def __init__(self):
         self.dataHora: str = ''
         self.ponto: str = ''
         self.placa: str = ''
         self.sentido: str = ''
+
+    def format_datetime_for_url(self, dt: datetime):
+        return dt.strftime("%Y.%m.%d"), dt.strftime("%H.%M.%S.000")
+
+
+class APSPortodeSantos(TratamentoLPR):
+
+    def __init__(self):
+        super().__init__()
         self.Latitude: str = ''
         self.Longitude: str = ''
         self.RecordNumber: str = ''
         self.Reliability: str = ''
         self.Hit: str = ''
         self.Confidence: str = ''
-    def parse_xml(self, lpr_record):
-        self.dataHora = lpr_record.find('DateTime').text
-        self.ponto = lpr_record.find('Camera').text
-        self.placa = lpr_record.find('LicensePlate').text
-        self.RecordNumber = lpr_record.find('RecordNumber').text
-        self.Latitude = lpr_record.find('Latitude').text
-        self.Longitude = lpr_record.find('Longitude').text
-        self.Reliability = lpr_record.find('Reliability').text
-        self.Hit = lpr_record.find('Hit').text
-        self.Confidence = lpr_record.find('Confidence').text
+
+    def parse_xml(self, alpr_record):
+        self.dataHora = alpr_record.find('DateTime').text
+        self.ponto = alpr_record.find('Camera').text
+        self.placa = alpr_record.find('LicensePlate').text
+        self.RecordNumber = alpr_record.find('RecordNumber').text
+        self.Latitude = alpr_record.find('Latitude').text
+        self.Longitude = alpr_record.find('Longitude').text
+        self.Reliability = alpr_record.find('Reliability').text
+        self.Hit = alpr_record.find('Hit').text
+        self.Confidence = alpr_record.find('Confidence').text
 
     def to_sivana(self):
         info = f'Reliability:{self.Reliability} - ' + \
@@ -39,15 +51,36 @@ class APSPortodeSantos():
         return dict_sivana
 
 
-
 if __name__ == '__main__':
+    # Definir as datas de início e fim - testar com a última hora
+    end_date = datetime.now()
+    start_date = end_date - timedelta(hours=1)
 
+    # Inicia o objeto de tradução do XML
+    aps_manager = APSPortodeSantos()
 
-# Parse the XML content
-root = ET.fromstring(xml_content)
+    # Formatar as datas para a URL
+    start_date_str, start_time_str = aps_manager.format_datetime_for_url(start_date)
+    end_date_str, end_time_str = aps_manager.format_datetime_for_url(end_date)
 
-# List to store the extracted records
-records = []
+    # Construir a URL dinamicamente
+    url = (
+        f"http://vms3.portodesantos.com.br:8601/Interface/LPR/Search?"
+        f"StartDate={start_date_str}&StartTime={start_time_str}&"
+        f"EndDate={end_date_str}&EndTime={end_time_str}"
+    )
 
-# Iterate over each LPRRecord
-for lpr_record in root.findall('.//LPRRecord'):
+    # Baixar o conteúdo XML
+    response = requests.get(url)
+
+    # Verificar se a requisição foi bem-sucedida
+    if response.status_code == 200:
+        # Parse do conteúdo XML
+        root = ET.fromstring(response.content)
+        # List to store the extracted records
+        records = []
+
+        # Iterate over each LPRRecord
+        for lpr_record in root.findall('.//LPRRecord'):
+            aps_manager.parse_xml(lpr_record)
+            print(aps_manager.to_sivana())
