e14d2eddedbaad9ac999429edf998f694d753aea
IvanBrasilico <brasilico.ivan@gmail.com>
2025-02-12 11:05:25 -0300

    Tratar endpoint da APS para Sivana - geração de JSON padrão Sivana
    Próximo passo validar os dados e mover código para módulos separados, com um código de script que chame tudo para produção.

--- virasana/integracao/sivana/aps_porto_de_santos.py
+++ virasana/integracao/sivana/aps_porto_de_santos.py
@@ -1,7 +1,32 @@
+import csv
+import json
+import os
+import sys
 import xml.etree.ElementTree as ET
 from datetime import datetime, timedelta
+from typing import Tuple
 
 import requests
+
+sys.path.append('../ajna_docs/commons')
+
+from ajna_commons.flask.log import logger
+
+CSV_FILE = 'lpr_credentials.csv'
+
+
+# Função para encontrar uma entrada pela classe
+def get_credentials(lpr_name: str) -> dict:
+    if not os.path.isfile(CSV_FILE):
+        logger.error(f'Arquivo CSV {CSV_FILE} não encontrado!!')
+        return {}
+    with open(CSV_FILE, mode="r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if row["class"] == lpr_name:
+                return row  # Retorna a linha encontrada
+    logger.error(f'Nenhuma entrada encontrada para a ALPR: {lpr_name}.')
+    return {}
 
 
 class TratamentoLPR:
@@ -10,9 +35,34 @@ class TratamentoLPR:
         self.ponto: str = ''
         self.placa: str = ''
         self.sentido: str = ''
+        credentials = get_credentials(type(self).__name__)
+        self.username = credentials.get('username')
+        self.password = credentials.get('password')
 
-    def format_datetime_for_url(self, dt: datetime):
-        return dt.strftime("%Y.%m.%d"), dt.strftime("%H.%M.%S.000")
+    def get_url(self, astart_date: datetime, aend_date: datetime) -> str:
+        raise NotImplementedError('get_url deve ser implementado!')
+
+    def get(self, url):
+        '''
+        Acopla o requests na classe, pois esta tem conhecimento da autenticação
+        Com o tempo, confirmar se é necessário este acoplamente
+
+        Returns: requests response object
+        '''
+        response = requests.get(aps_manager.get_url(start_date, end_date),
+                                auth=(aps_manager.username, aps_manager.password))
+
+    def format_datetime_for_url(self, dt: datetime) -> Tuple[str, str]:
+        raise NotImplementedError('format_datetime_for_url deve ser implementado!')
+
+    def parse_xml(self, alpr_record):
+        """
+        Retorna o próprio objeto "self" para permitir uso encadeado com to_sivana
+
+        Args:
+            alpr_record: XML de uma ALPR (estação de reconhecimento de placa) adquirida
+        """
+        raise NotImplementedError('parse_xml deve ser implementado!')
 
 
 class APSPortodeSantos(TratamentoLPR):
@@ -26,6 +76,23 @@ class APSPortodeSantos(TratamentoLPR):
         self.Hit: str = ''
         self.Confidence: str = ''
 
+    def format_datetime_for_url(self, dt: datetime) -> Tuple[str, str]:
+        return dt.strftime("%Y.%m.%d"), dt.strftime("%H.%M.%S.000")
+
+    def get_url(self, astart_date: datetime, aend_date: datetime) -> str:
+        # Formatar as datas para a URL
+        start_date_str, start_time_str = self.format_datetime_for_url(astart_date)
+        end_date_str, end_time_str = self.format_datetime_for_url(aend_date)
+        url = (
+            f"http://vms3.portodesantos.com.br:8601/Interface/LPR/Search?"
+            f"StartDate={start_date_str}&StartTime={start_time_str}&"
+            f"EndDate={end_date_str}&EndTime={end_time_str}"
+        )
+        return url
+
+    def get(self, url):
+        return requests.get(url, auth=(self.username, self.password))
+
     def parse_xml(self, alpr_record):
         self.dataHora = alpr_record.find('DateTime').text
         self.ponto = alpr_record.find('Camera').text
@@ -36,22 +103,27 @@ class APSPortodeSantos(TratamentoLPR):
         self.Reliability = alpr_record.find('Reliability').text
         self.Hit = alpr_record.find('Hit').text
         self.Confidence = alpr_record.find('Confidence').text
+        return self
 
     def to_sivana(self):
         info = f'Reliability:{self.Reliability} - ' + \
                f'Hit: {self.Hit} - ' + \
-               f'Confidence: {self.Confidence}'
+               f'Confidence: {self.Confidence} - ' + \
+               f'Latitude: {self.Latitude} - ' + \
+               f'Longitude: {self.Longitude}'
         dict_sivana = {
             'placa': self.placa,
             'ponto': self.ponto,
             'sentido': self.sentido,
-            'dataHora': self.dataHora.strftime('%Y-%m-%dT%H:%M:%S'),
+            'dataHora': self.dataHora,  # self.dataHora.strftime('%Y-%m-%dT%H:%M:%S'),
             'info': info
         }
         return dict_sivana
 
 
 if __name__ == '__main__':
+    # username = input('Usuário:')
+    # password = input('Password:')
     # Definir as datas de início e fim - testar com a última hora
     end_date = datetime.now()
     start_date = end_date - timedelta(hours=1)
@@ -59,28 +131,37 @@ if __name__ == '__main__':
     # Inicia o objeto de tradução do XML
     aps_manager = APSPortodeSantos()
 
-    # Formatar as datas para a URL
-    start_date_str, start_time_str = aps_manager.format_datetime_for_url(start_date)
-    end_date_str, end_time_str = aps_manager.format_datetime_for_url(end_date)
-
-    # Construir a URL dinamicamente
-    url = (
-        f"http://vms3.portodesantos.com.br:8601/Interface/LPR/Search?"
-        f"StartDate={start_date_str}&StartTime={start_time_str}&"
-        f"EndDate={end_date_str}&EndTime={end_time_str}"
-    )
-
     # Baixar o conteúdo XML
-    response = requests.get(url)
+    response = aps_manager.get(aps_manager.get_url(start_date, end_date))
 
     # Verificar se a requisição foi bem-sucedida
-    if response.status_code == 200:
-        # Parse do conteúdo XML
-        root = ET.fromstring(response.content)
-        # List to store the extracted records
-        records = []
+    if response.status_code != 200:
+        sys.exit(f'Erro:{response.status_code}, {response.text}')
 
-        # Iterate over each LPRRecord
-        for lpr_record in root.findall('.//LPRRecord'):
-            aps_manager.parse_xml(lpr_record)
-            print(aps_manager.to_sivana())
+    # Parse do conteúdo XML
+    root = ET.fromstring(response.content)
+    # Iterate over each LPRRecord and put result in a list
+    records = [aps_manager.parse_xml(lpr_record).to_sivana()
+               for lpr_record in root.findall('.//LPRRecord')]
+    payload = {'totalLinhas': len(records), 'offset': '-03:00', 'passagens': records}
+    formated = json.dumps(payload, indent=4)
+    print(formated)
+    with open('APS.json', 'w') as f:
+        json.dump(formated, f)
+
+# TODO: incluir código para transmitir para Sivana, após validar mecanismo de salvar
+# avanço (última transmissão) e validar os dados
+''' 
+if passagens and len(passagens) > 0:
+    r = post(URL_API_SIVANA, pkcs12_filename=PKCS12_FILENAME,
+             pkcs12_password=SENHA_PCKS_SIVANA, json=payload, verify=False)
+    if r.status_code != 200:
+        logger.error(f'ERRO {r.status_code} no Upload para Sivana: {r.text}')
+    else:
+        # Verifica se houve acessos e pega o maior ID
+        maior_id = max(acesso.id for acesso in acessos)
+        # Atualiza o arquivo com o maior ID encontrado
+        grava_ultimo_id_transmitido(maior_id)
+else:
+    logger.info(f'Não há novos registros de acesso a transmitir')
+'''
