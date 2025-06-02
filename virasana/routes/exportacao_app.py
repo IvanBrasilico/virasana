5e453ecf1ac1bb9e64bc1b8fba0e62f9a8e366f7
Dimitri Sean Ribeiro Carneiro <dimitri.carneiro@rfb.gov.br>
2025-06-02 11:56:23 -0300

    Comenta linhas da rota de exportação

--- virasana/routes/exportacao_app.py
+++ virasana/routes/exportacao_app.py
@@ -1,7 +1,7 @@
 # exportacao_app.py
 
-from datetime import date, timedelta, datetime, time
-from bhadrasana.main import mongodb
+#from datetime import date, timedelta, datetime, time
+#from bhadrasana.main import mongodb
 from flask import render_template, request, flash, url_for
 from flask import Blueprint, render_template
 from flask_wtf.csrf import generate_csrf
@@ -25,79 +25,79 @@ def index():
 
 
 
-def get_imagens_container_data(mongodb, numero, inicio_scan, fim_scan, vazio=False) -> list:
-    query = {
-        'metadata.contentType': 'image/jpeg',
-        'metadata.dataescaneamento': {'$gte': inicio_scan, '$lte': fim_scan}
-    }
+#def get_imagens_container_data(mongodb, numero, inicio_scan, fim_scan, vazio=False) -> list:
+#    query = {
+#        'metadata.contentType': 'image/jpeg',
+#        'metadata.dataescaneamento': {'$gte': inicio_scan, '$lte': fim_scan}
+#    }
 
     # Adiciona o filtro por número apenas se fornecido
-    if numero:
-        query['metadata.numeroinformado'] = numero
+#    if numero:
+#        query['metadata.numeroinformado'] = numero
 
-    projection = {
-        'metadata.numeroinformado': 1,
-        'metadata.dataescaneamento': 1,
-        'metadata.predictions.vazio': 1
-    }
+#    projection = {
+#        'metadata.numeroinformado': 1,
+#        'metadata.dataescaneamento': 1,
+#        'metadata.predictions.vazio': 1
+#    }
 
-    cursor = (
-        mongodb['fs.files']
-        .find(query, projection)
-        .sort('metadata.dataescaneamento', -1)
-        .limit(10)
-    )
+#    cursor = (
+#        mongodb['fs.files']
+#        .find(query, projection)
+#        .sort('metadata.dataescaneamento', -1)
+#        .limit(10)
+#    )
 
-    return list(cursor)
+#    return list(cursor)
 
 
 
-def get_imagens_container_sem_data(mongodb, numero, vazio=False) -> list:
-    query = {
-        'metadata.contentType': 'image/jpeg',
-    }
+#def get_imagens_container_sem_data(mongodb, numero, vazio=False) -> list:
+#    query = {
+#        'metadata.contentType': 'image/jpeg',
+#    }
 
     # Adiciona o filtro por número apenas se fornecido
-    if numero:
-        query['metadata.numeroinformado'] = numero
+#    if numero:
+#        query['metadata.numeroinformado'] = numero
 
-    projection = {
-        'metadata.numeroinformado': 1,
-        'metadata.dataescaneamento': 1,
-        'metadata.predictions.vazio': 1
-    }
+#    projection = {
+#        'metadata.numeroinformado': 1,
+#        'metadata.dataescaneamento': 1,
+#        'metadata.predictions.vazio': 1
+#    }
 
-    cursor = (
-        mongodb['fs.files']
-        .find(query, projection)
-        .sort('metadata.dataescaneamento', -1)
-        .limit(10)
-    )
+#    cursor = (
+#        mongodb['fs.files']
+#        .find(query, projection)
+#        .sort('metadata.dataescaneamento', -1)
+#        .limit(10)
+#    )
 
-    return list(cursor)
+#    return list(cursor)
 
 
-@exportacao_app.route('/stats', methods=['POST'])
-def stats():
-    numero = request.form.get('numero')
-    start = request.form.get('start')
-    end = request.form.get('end')
+#@exportacao_app.route('/stats', methods=['POST'])
+#def stats():
+#    numero = request.form.get('numero')
+#    start = request.form.get('start')
+#    end = request.form.get('end')
 
-    if not start and not end and numero:
-        arquivos = get_imagens_container_sem_data(mongodb, numero)
-        return render_template(
-            'exportacao.html',
-            arquivos=arquivos,
-            csrf_token=generate_csrf
-        )
+#    if not start and not end and numero:
+#        arquivos = get_imagens_container_sem_data(mongodb, numero)
+#        return render_template(
+#            'exportacao.html',
+#            arquivos=arquivos,
+#            csrf_token=generate_csrf
+#        )
 
-    inicio_scan = datetime.strptime(start, '%Y-%m-%d')
-    fim_scan = datetime.strptime(end, '%Y-%m-%d')
+#    inicio_scan = datetime.strptime(start, '%Y-%m-%d')
+#    fim_scan = datetime.strptime(end, '%Y-%m-%d')
 
-    arquivos = get_imagens_container_data(mongodb, numero, inicio_scan, fim_scan)
+#    arquivos = get_imagens_container_data(mongodb, numero, inicio_scan, fim_scan)
 
-    return render_template(
-        'exportacao.html',
-        arquivos=arquivos,
-        csrf_token=generate_csrf
-    )
+#    return render_template(
+#        'exportacao.html',
+#        arquivos=arquivos,
+#        csrf_token=generate_csrf
+#    )
