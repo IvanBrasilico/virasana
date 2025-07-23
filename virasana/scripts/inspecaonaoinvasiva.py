6ba35339604c57f7cd4115c821bb88ccab4cba86
IvanBrasilico <brasilico.ivan@gmail.com>
2025-07-23 17:31:40 -0300

    Códigos de Upload de ZIP testados com formato criado por Mariana no script contágil hoje às 17h

--- virasana/scripts/inspecaonaoinvasiva.py
+++ virasana/scripts/inspecaonaoinvasiva.py
@@ -1,11 +1,13 @@
 import io
 import json
 import zipfile
+from datetime import datetime
 
 import requests
 
 
 def upload(url, files, data):
+    print(f'Uploading {data}')
     r = requests.post(url, files=files, data=data, verify=False)
     print(f'Status code: {r.status_code}')
     print('Response:', r.json())
@@ -23,48 +25,44 @@ def upload_file(url, image_path, json_path):
     }}
     print(image_path)
     with open(image_path, 'rb') as img_file:
-        files = {
-            'imagem': (image_path, img_file, 'image/jpeg')
-        }
-        data = {
-            'json': json.dumps(json_data)
-        }
+        files = {'imagem': (image_path, img_file, 'image/jpeg')}
+        data = {'json': json.dumps(json_data)}
         return upload(url, files, data)
 
 
-def upload_zip(zip_filename):
+def upload_zip(url, zip_filename):
     with zipfile.ZipFile(open(zip_filename, 'rb')) as zip_file:
         for file_info in zip_file.infolist():
             filename = file_info.filename
             base_name = filename.rsplit('.', 1)[0]
             parts = base_name.split('-')
-            if len(parts) < 3:
+            if len(parts) < 4:
                 print(f'Arquivo {filename} não tem as informações esperadas no nome')
                 continue  # Não tem metadata no nome conforme esperado
-            numeroinformado, recinto, data_str = parts[0], parts[1], parts[2]
+            recinto, numeroinformado, data_str, time_str = parts[0], parts[1], parts[2], parts[3]
+            datetime_str = datetime.strptime(data_str + time_str, "%d%m%Y%H%M").strftime("%Y-%m-%dT%H:%M:%S-0300")
             json_data = {'jsonOriginal': {
-                "dataHoraOcorrencia": data_str,
+                "dataHoraOcorrencia": datetime_str,
                 "codigoRecinto": recinto,
                 "listaConteineresUld": [
                     {"numeroConteiner": numeroinformado}
                 ]
             }}
+            data = {'json': json.dumps(json_data)}
             with zip_file.open(file_info) as file:
                 file_bytes = io.BytesIO(file.read())
-            files = {
-                'imagem': (image_path, file_bytes, 'image/jpeg')
-            }
-            upload(url, files, json_data)
+            files = {'imagem': (filename, file_bytes, 'image/jpeg')}
+            upload(url, files, data)
 
 
 if __name__ == '__main__':
     # TODO: load from_zip, filename, url, image_path, json_path from args
+    url = 'http://localhost:5001/api/inspecaonaoinvasiva'
+    # url = 'https://ajna1.rfoc.srf/virasana/api/inspecaonaoinvasiva'
+    filename = "C:/Users/25052288840/Downloads/8931305-2025-07-23_imagens.zip"
     from_zip = True
+    # filename = 'C:/Users/25052288840/Downloads/MSMU2186544-17072025-2258.jpg'
     if from_zip:
-        filename = "inspecoes.zip"
-        upload_zip(filename)
+        upload_zip(url, filename)
     else:
-        # url = 'https://ajna1.rfoc.srf/virasana/api/inspecaonaoinvasiva'
-        url = 'http://localhost:5001/api/inspecaonaoinvasiva'
-        image_path = 'C:/Users/25052288840/Downloads/MSMU2186544-17072025-2258.jpg'
-        upload_file(url, image_path, None)
+        upload_file(url, filename, None)
