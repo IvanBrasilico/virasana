aed582c67b8c9f803b7c9df2eaa8ed91b2be0438
IvanBrasilico <brasilico.ivan@gmail.com>
2025-10-23 14:38:41 -0300

    __main__

--- virasana/scripts/email.py
+++ virasana/scripts/email.py
@@ -1,15 +1,16 @@
 import win32com.client
 
-# Inicia uma instância do Outlook
-outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+if __name__ == '__main__':
+    # Inicia uma instância do Outlook
+    outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
 
-# Acessa a caixa de entrada padrão
-inbox = outlook.GetDefaultFolder(6)  # 6 = inbox
+    # Acessa a caixa de entrada padrão
+    inbox = outlook.GetDefaultFolder(6)  # 6 = inbox
 
-# Pega os emails
-messages = inbox.Items
-for message in messages:
-    print("Assunto:", message.Subject)
-    print("Remetente:", message.SenderName)
-    print("Data:", message.ReceivedTime)
-    print("Corpo:", message.Body[:200], '\n')  # Print dos primeiros caracteres do corpo
+    # Pega os emails
+    messages = inbox.Items
+    for message in messages:
+        print("Assunto:", message.Subject)
+        print("Remetente:", message.SenderName)
+        print("Data:", message.ReceivedTime)
+        print("Corpo:", message.Body[:200], '\n')  # Print dos primeiros caracteres do corpo
