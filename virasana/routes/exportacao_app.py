22e14aec5bcaf2747e2434e3444e71b9e8aa9f4d
33380367877 <dimitri.carneiro@rfb.gov.br>
2025-11-06 10:38:45 -0300

    style: reorganização do código

--- virasana/routes/exportacao_app.py
+++ virasana/routes/exportacao_app.py
@@ -13,8 +13,6 @@ import csv
 import os
 from io import StringIO
 
-DEFAULT_LOOKBACK_DAYS = 30  # usado quando numero= e não há de/ate/data
-
 from pathlib import Path
 import tempfile
 from io import BytesIO
@@ -24,6 +22,8 @@ from pymongo import ASCENDING
 from bson import ObjectId
 from gridfs import GridFS
 from PIL import Image
+
+DEFAULT_LOOKBACK_DAYS = 30  # usado quando numero= e não há de/ate/data
 
 
 def configure(app):
@@ -471,94 +471,94 @@ def configure(app):
             csrf_token=generate_csrf
         )
 
-    def get_imagens_container_data(
-        mongodb,
-        numero,
-        inicio_scan,
-        fim_scan,
-        vazio=False
-    ) -> list:
-        query = {
-            'metadata.contentType': 'image/jpeg',
-            'metadata.dataescaneamento': {'$gte': inicio_scan, '$lte': fim_scan}
-        }
-
-        # Adiciona o filtro por número apenas se fornecido
-        if numero:
-            query['metadata.numeroinformado'] = numero
-
-        projection = {
-            'metadata.numeroinformado': 1,
-            'metadata.dataescaneamento': 1,
-            'metadata.predictions.vazio': 1
-        }
-
-        cursor = (
-            mongodb['fs.files']
-            .find(query, projection)
-            .sort('metadata.dataescaneamento', -1)
-            .limit(10)
-        )
-
-        return list(cursor)
-
-    def get_imagens_container_sem_data(mongodb, numero, vazio=False) -> list:
-        query = {
-            'metadata.contentType': 'image/jpeg',
-        }
-
-        # Adiciona o filtro por número apenas se fornecido
-        if numero:
-            query['metadata.numeroinformado'] = numero
-
-        projection = {
-            'metadata.numeroinformado': 1,
-            'metadata.dataescaneamento': 1,
-            'metadata.predictions.vazio': 1
-        }
-
-        cursor = (
-            mongodb['fs.files']
-            .find(query, projection)
-            .sort('metadata.dataescaneamento', -1)
-            .limit(10)
-        )
-
-        return list(cursor)
-
-    @app.route('/exportacao/stats', methods=['POST'])
-    def exportacao_stats():
-
-        mongodb = app.config['mongodb']
-        session = app.config['db_session']
-
-        numero = request.form.get('numero')
-        start = request.form.get('start')
-        end = request.form.get('end')
-
-        if not start and not end and numero:
-            arquivos = get_imagens_container_sem_data(mongodb, numero)
-            return render_template(
-                'exportacao.html',
-                arquivos=arquivos,
-                csrf_token=generate_csrf
-            )
-
-        inicio_scan = datetime.strptime(start, '%Y-%m-%d')
-        fim_scan = datetime.strptime(end, '%Y-%m-%d')
-
-        arquivos = get_imagens_container_data(
-            mongodb,
-            numero,
-            inicio_scan,
-            fim_scan,
-        )
-
-        return render_template(
-            'exportacao.html',
-            arquivos=arquivos,
-            csrf_token=generate_csrf
-        )
+#    def get_imagens_container_data(
+#        mongodb,
+#        numero,
+#        inicio_scan,
+#        fim_scan,
+#        vazio=False
+#    ) -> list:
+#        query = {
+#            'metadata.contentType': 'image/jpeg',
+#            'metadata.dataescaneamento': {'$gte': inicio_scan, '$lte': fim_scan}
+#        }
+#
+#        # Adiciona o filtro por número apenas se fornecido
+#        if numero:
+#            query['metadata.numeroinformado'] = numero
+#
+#        projection = {
+#            'metadata.numeroinformado': 1,
+#            'metadata.dataescaneamento': 1,
+#            'metadata.predictions.vazio': 1
+#        }
+#
+#        cursor = (
+#            mongodb['fs.files']
+#            .find(query, projection)
+#            .sort('metadata.dataescaneamento', -1)
+#            .limit(10)
+#        )
+#
+#        return list(cursor)
+#
+#    def get_imagens_container_sem_data(mongodb, numero, vazio=False) -> list:
+#        query = {
+#            'metadata.contentType': 'image/jpeg',
+#        }
+#
+#        # Adiciona o filtro por número apenas se fornecido
+#        if numero:
+#            query['metadata.numeroinformado'] = numero
+#
+#        projection = {
+#            'metadata.numeroinformado': 1,
+#            'metadata.dataescaneamento': 1,
+#            'metadata.predictions.vazio': 1
+#        }
+#
+#        cursor = (
+#            mongodb['fs.files']
+#            .find(query, projection)
+#            .sort('metadata.dataescaneamento', -1)
+#            .limit(10)
+#        )
+#
+#        return list(cursor)
+#
+#    @app.route('/exportacao/stats', methods=['POST'])
+#    def exportacao_stats():
+#
+#        mongodb = app.config['mongodb']
+#        session = app.config['db_session']
+#
+#        numero = request.form.get('numero')
+#        start = request.form.get('start')
+#        end = request.form.get('end')
+#
+#        if not start and not end and numero:
+#            arquivos = get_imagens_container_sem_data(mongodb, numero)
+#            return render_template(
+#                'exportacao.html',
+#                arquivos=arquivos,
+#                csrf_token=generate_csrf
+#            )
+#
+#        inicio_scan = datetime.strptime(start, '%Y-%m-%d')
+#        fim_scan = datetime.strptime(end, '%Y-%m-%d')
+#
+#        arquivos = get_imagens_container_data(
+#            mongodb,
+#            numero,
+#            inicio_scan,
+#            fim_scan,
+#        )
+#
+#        return render_template(
+#            'exportacao.html',
+#            arquivos=arquivos,
+#            csrf_token=generate_csrf
+#        )
 
     # ---------------------------------------------------------
     # Consulta de PESO: primeira pesagem válida (I/R) do contêiner
